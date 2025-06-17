7571220fa6483e00d8ac6cbf5d074504e9470434
anderson.arevalo@tht.company <anderson.arevalo@tht.company>
2025-06-16 21:15:06 -0500

    v1

--- Presidio/.history/src/utils/logger_20250522093150.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def setup_logger(name: str = __name__, level: int = logging.INFO) -> logging.Logger:
-    """Configura y retorna un logger"""
-    logging.basicConfig(
-        level=level,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-    return logging.getLogger(name)
